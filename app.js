cab290f26406ebf17591fe99e9cf6be4485fdaf0
jkinman <joel.kinman@gmail.com>
2018-01-06 13:11:10 -0800

    change logging

--- app.js
+++ app.js
@@ -141,7 +141,7 @@ var client = new Twitter({
 
   var stream = client.stream('statuses/filter', {track: 'vancouver'});
   stream.on('data', function(event) {
-    console.log(event.text);
+    console.log(event);
     var dataPacket = {profile: event.user.profile_image_url, 
         text: event.text, 
         desc: event.user.description, 
